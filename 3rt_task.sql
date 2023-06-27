d273731b8aef71780e5fe4939bd42c99504c87c4
ACupOfWhiskey <mia.nishida@gmail.com>
2023-06-27 06:12:59 +0300

    complete 3th homework lesson

---
+++ 3rt_task.sql
@@ -0,0 +1,78 @@
+DROP DATABASE IF EXISTS 3th_homework_task;
+
+CREATE DATABASE IF NOT EXISTS 3th_homework_task;
+
+USE 3th_homework_task;
+
+CREATE TABLE IF NOT EXISTS `Staff`
+(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `first_name` VARCHAR(45),
+    `last_name` VARCHAR(45),
+    `post` VARCHAR(45),
+    `seniority` INT,
+    `salary` INT,
+    `age` INT
+);
+
+INSERT INTO `Staff` (first_name, last_name, post, seniority, salary, age)
+VALUES 
+ ('Вася', 'Петров', 'Начальник', 40, 100000, 60),
+ ('Петр', 'Власов', 'Начальник', 8, 70000, 30),
+ ('Катя', 'Катина', 'Инженер', 2, 70000, 25),
+ ('Саша', 'Сасин', 'Инженер', 12, 50000, 35),
+ ('Иван', 'Иванов', 'Рабочий', 40, 30000, 59),
+ ('Петр', 'Петров', 'Рабочий', 20, 25000, 40),
+ ('Сидр', 'Сидоров', 'Рабочий', 10, 20000, 35),
+ ('Антон', 'Антонов', 'Рабочий', 8, 19000, 28),
+ ('Юрий', 'Юрков', 'Рабочий', 5, 15000, 25),
+ ('Максим', 'Максимов', 'Рабочий', 2, 11000, 22),
+ ('Юрий', 'Галкин', 'Рабочий', 3, 12000, 24),
+ ('Людмила', 'Маркина', 'Уборщик', 10, 10000, 49);
+
+
+-- 1. Отсортируйте данные по полю заработная плата (salary) в порядке: убывания; возрастания
+-- по убыванию:
+SELECT *
+FROM Staff
+ORDER BY salary DESC;
+-- по возрастанию: 
+SELECT *
+FROM Staff
+ORDER BY salary;
+
+
+-- 2. Выведите 5 максимальных заработных плат (saraly)
+SELECT salary
+FROM Staff
+ORDER BY salary DESC
+LIMIT 5;
+
+
+-- 3. Посчитайте суммарную зарплату (salary) по каждой специальности (роst)
+SELECT 
+	post,
+    SUM(salary) AS sum_salary
+FROM Staff
+GROUP BY post;
+
+
+-- 4. Найдите кол-во сотрудников с специальностью (post) «Рабочий» в возрасте от 24 до 49 лет включительно.
+SELECT
+	COUNT(id) AS count_id
+FROM Staff
+WHERE post = "Рабочий" AND age > 24 AND age <= 49;
+
+
+-- 5. Найдите количество специальностей
+SELECT 
+	COUNT(DISTINCT post) AS count_post
+FROM Staff;
+
+
+-- 6. Выведите специальности, у которых средний возраст сотрудников меньше 30 лет 
+SELECT
+	post
+FROM Staff
+GROUP BY post
+HAVING AVG(age) < 30;
